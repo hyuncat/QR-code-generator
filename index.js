fff3f00f6e85d19b9d0b0c14c9a6926e6fc3b787
sarah <ssh2198@columbia.edu>
2024-04-11 01:31:24 -0400

    Yay QR barkbox

---
+++ index.js
@@ -0,0 +1,33 @@
+const qr = require('qr-image');
+const fs = require('fs');
+
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+async function main() {
+  const inquirerModule = await import('inquirer');
+  const inquirer = inquirerModule.default;
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'url',
+        message: 'Please enter a URL to generate a QR code:',
+      },
+    ])
+    .then((answers) => {
+      const qr_png = qr.image(answers.url, { type: 'png' });
+      qr_png.pipe(fs.createWriteStream('qr_img.png'));
+
+      fs.writeFile('URL.txt', answers.url, (err) => {
+        if (err) throw err;
+        console.log('\nThe file has been saved!\n---\n - To view the QR code, open qr_img.png\n - To view the URL, open URL.txt\n - To generate a new QR code, restart the program.');
+      });
+    });
+}
+
+main();
